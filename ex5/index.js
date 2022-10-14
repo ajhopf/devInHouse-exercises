6196a4fbdf28e0e8f4f2f13e9c0604a45cb7ddfe
André Hopf <andre.hopf@gmail.com>
2022-10-14 09:35:43 -0300

    Exercicio 5 concluido

---
+++ ex5/index.js
@@ -0,0 +1,30 @@
+let number = NaN;
+
+while (isNaN(number)) {
+  number = parseInt(prompt('Digite um número'));
+}
+
+const numeroDeParImpar = () => {
+  const numeroInicial = number;
+
+  let pares = 0;
+  let impares = 0;
+
+  if (number < 0) {
+    number *= -1;
+  }
+
+  for (number; number > 0; number--) {
+    if (number % 2 === 0) {
+      pares++;
+    } else {
+      impares++;
+    }
+  }
+
+  alert(
+    `Do 0 até o número ${numeroInicial} existem ${pares} números pares e ${impares} números ímpares.`
+  );
+};
+
+numeroDeParImpar();
---
+++ ex5/readme.md
@@ -0,0 +1,4 @@
+Instruções
+
+Exiba um prompt solicitando a entrada de um número para saber quantos pares e impares existem de 0 até o número digitado.
+Calcule o número de pares e impares e exiba o resultado utilizando um alert.
